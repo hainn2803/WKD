c735504074394d24dec9fb160b779bf08e98c0a8
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2025-04-07 08:08:55 +0000

    h

--- mdistiller/dataset/imagenet.py
+++ mdistiller/dataset/imagenet.py
@@ -5,7 +5,7 @@ from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
 
 data_folder = 'mdistiller/dataset/data/imagenet'
-data_folder = "/lustre/scratch/client/movian/research/users/hainn14/WKD/mdistiller/dataset/data/imagenet"
+data_folder = "/lustre/scratch/client/movian/research/users/hainn14/dataset/imagenet"
 
 
 class ImageNet(ImageFolder):
