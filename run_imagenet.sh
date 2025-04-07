c165cc50dee95cce076bc31340d84c84b943e720
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2025-04-07 09:16:38 +0000

    h

--- run_imagenet.sh
+++ run_imagenet.sh
@@ -1,1 +1,22 @@
+#!/bin/bash -e
+#SBATCH --job-name=img_base
+#SBATCH --output=/lustre/scratch/client/movian/research/users/hainn14/WKD/spp_noti/imagnet_baseline.out
+#SBATCH --error=/lustre/scratch/client/movian/research/users/hainn14/WKD/spp_noti/imagnet_baseline.err
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=125G
+#SBATCH --cpus-per-gpu=32
+#SBATCH --partition=movianr
+#SBATCH --mail-type=all
+#SBATCH --mail-user=v.HaiNN14@vinai.io
+
+module purge
+module load python/miniconda3/miniconda3
+
+# Corrected line
+eval "$(conda shell.bash hook)"
+
+conda activate /lustre/scratch/client/movian/research/users/hainn14/envs/wkd
+cd /lustre/scratch/client/movian/research/users/hainn14/WKD
+
 WANDB_MODE=disabled python tools/train.py --cfg configs/imagenet/r34_r18/wkd_f.yaml
